f33e6b4f02dad3841edb3ef3831b3892367c39e7
Krishal <kparikh.6400@gmail.com>
2017-11-22 02:01:13 -0500

    Trying to add objects

---
+++ app/src/main/java/krishal/com/myworkout/Cardio.java
@@ -0,0 +1,11 @@
+package krishal.com.myworkout;
+
+/**
+ * Created by Krishal on 2017-11-22.
+ */
+
+public class Cardio extends Exercise{
+    public Cardio(String name){
+        super(name);
+    }
+}
---
+++ app/src/main/java/krishal/com/myworkout/Exercise.java
@@ -0,0 +1,13 @@
+package krishal.com.myworkout;
+
+/**
+ * Created by Krishal on 2017-11-22.
+ */
+
+public abstract class Exercise {
+    private String name;
+
+    public Exercise(String name){
+        this.name = name;
+    }
+}
